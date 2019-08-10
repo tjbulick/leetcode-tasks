65189504369134efd72978097631effabeabc3a2
tjbulick <e.pichtchalnikov@gmail.com>
2019-08-11 00:30:39 +0300

    typos & comments

--- 1122.js
+++ 1122.js
@@ -1,7 +1,7 @@
 const relativeSortArray = (arr1, arr2) => {
 	let leftPart = [];
 
-	// walk through first array by arr2's entries
+	// walk through the first array by arr2's entries
 	for (const elem of arr2) {
 		while (arr1.indexOf(elem) !== -1) {
 			const index = arr1.indexOf(elem);
@@ -9,9 +9,11 @@ const relativeSortArray = (arr1, arr2) => {
 		}
 	}
 
+	// sort remaining elements
 	arr1.sort((a, b) => (a - b));
 	
+	// put two parts together
 	return leftPart.concat(arr1);
 }
 
-console.log(relativeSortArray([2,3,1,3,2,4,6,7,9,2,19], [2,1,4,3,9,6]))
+console.log(relativeSortArray([2,3,1,3,2,4,6,7,9,2,19], [2,1,4,3,9,6]));
