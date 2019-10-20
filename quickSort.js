10e15aa423dcf27a3b8ca2aafcad2e2dd03c135a
tjbulick <e.pichtchalnikov@gmail.com>
2019-10-20 16:01:23 +0300

    Add quick sort algorithm

---
+++ quickSort.js
@@ -0,0 +1,19 @@
+const quickSort = (arr) => {
+	if (arr.length < 2) {
+		return arr;
+	} else {
+		const pivotPosition = Math.floor(Math.random() * arr.length);
+		const pivot = arr[pivotPosition];
+
+		const less = arr.filter(value => value < pivot);
+		const greater = arr.filter((value, index) => {
+			const isPivot = index === pivotPosition;
+			return !isPivot && (value >= pivot);
+		});
+
+		return [...quickSort(less), pivot, ...quickSort(greater)];
+	}
+}
+
+const arr = [1, 213, 3, 5, 2, 8, 7];
+console.log(quickSort(arr));
